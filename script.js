b72c5ed756411af3c7a0943af7e5cf7fe2d81c1d
ruthgonzalez2-wq <ruthgonzalezpersonal2@gmail.com>
2026-04-08 19:39:34 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+ 
+body {
+  margin: 0;
+  font-family: Arial, sans-serif;
+  background: #0f0f0f;
+  color: white;
+  text-align: center;
+}
+
+.container {
+  margin-top: 80px;
+}
+
+h1 {
+  color: #00ffcc;
+}
+
+button {
+  padding: 20px 40px;
+  font-size: 22px;
+  background: #00ffcc;
+  border: none;
+  cursor: pointer;
+  border-radius: 10px;
+  margin-top: 20px;
+}
+
+button:hover {
+  background: #00c9a3;
+}
+
+.stats {
+  margin: 20px;
+}
+
+#result {
+  font-size: 20px;
+  margin-top: 20px;
+}
